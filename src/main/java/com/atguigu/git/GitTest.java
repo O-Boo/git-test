f66f83c16dd8cf397f43d9e0e13005a567d25671
Bo <1372368449@qq.com>
2022-11-08 10:48:43 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @author Tonny
+ * @version 2021.2
+ * @date 2022/11/8 10:42
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,git!");
+    }
+}
